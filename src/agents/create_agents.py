614bce9bbcf820fd27555e9d44c427aa3a783693
allemar92 <allemar@live.com>
2025-09-29 21:36:04 +0200

    create agents

--- src/agents/create_agents.py
+++ src/agents/create_agents.py
@@ -1,14 +1,40 @@
 from clients.litellm_client import LiteLLMClient
 from agents.agent import Agent
 
-lite_client = LiteLLMClient(model="gpt-4o-mini", temperature=0.2, max_tokens=1024)
-client = lite_client.get_client()
+def get_client(model: str = "gpt-4o-mini", temperature: float = 0.2, max_tokens: int = 1024):
+    """
+    Returns a LiteLLM client with chosen parameters.
+    """
+    lite_client = LiteLLMClient(model=model, temperature=temperature, max_tokens=max_tokens)
+    return lite_client.get_client()
 
-#first agent Plant Selector
-plant_selector = Agent(
-    name="PlantSelector",
-    model="gpt-4o-mini",
-    role=" a helpful gardening assistant specialized in synergistic gardening."
+
+def create_plant_selector():
+    """
+    Returns the gardening PlantSelector agent.
+    """
+    return Agent(
+        name="PlantSelector",
+        model="gpt-4o-mini",
+        role="a helpful gardening assistant specialized in synergistic gardening."
     )
 
+def create_synergy_agent():
+    """
+    Returns the plant's synergy expert agent.
+    """
+    return Agent(
+        name="SynergyAgent",
+        model="gpt-4o-mini",
+        role="a helpful gardening assistant specialized in synergistic gardening."
+    )
 
+def create_garden_agent():
+    """
+    Returns the gardening agent
+    """
+    return Agent(
+        name="GardenAgent",
+        model="gpt-4o-mini",
+        role="a helpful gardening assistant specialized in synergistic gardening."
+    )
